4a13d75522b8affef24163539d324c2518370911
sboukiou <boukdsaid@gmail.com>
2025-04-21 16:43:00 +0100

    some fixes;

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"williamboman/mason.nvim",
 		opts = { ensure_installed = { "clangd", "lua-language-serve", "clang-format" } },
+		virtual_text = true;
 		config = function()
 			require("mason").setup()
 		end,
@@ -13,6 +14,14 @@ return {
 			-- local.
 			lspconfig.lua_ls.setup({})
 			lspconfig.clangd.setup({})
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				update_in_insert = false,
+				underline = true,
+				severity_sort = false,
+				float = true,
+			})
 
 		end,
 	},
