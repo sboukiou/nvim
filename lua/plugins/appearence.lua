80658d2b6425710f8ae3ee8e75d619b40b130776
sboukiou <sboukiou@student.1337.ma>
2026-01-02 09:53:05 +0100

    Added the lualine plugin

--- lua/plugins/appearence.lua
+++ lua/plugins/appearence.lua
@@ -11,12 +11,25 @@ return {
 		'nvim-treesitter/nvim-treesitter',
 		require("lazy").setup({
 			{
-				"nvim-treesitter/nvim-treesitter", 
+				"nvim-treesitter/nvim-treesitter",
 				branch = 'master',
 				lazy = false,
 				build = ":TSUpdate"
 			}
 		})
 
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('lualine').setup({
+				theme = 'iceberg_dark',
+				options = {
+					section_separators = { left = '', right = '' },
+					component_separators = { left = '', right = '' }
+				}
+			})
+		end,
 	},
 }
